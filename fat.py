2894ef7bf5c1487d6c977bfee84f5598e760d7e8
sa3mlk <jonas.engstrom@gmail.com>
2022-11-29 20:32:27 +0100

    Now seems to run in Python3

--- fat.py
+++ fat.py
@@ -10,241 +10,247 @@ from os import SEEK_SET
 #  etc.
 
 class FAT(object):
-	Version = "0.01"
+    Version = "0.01"
 
-	# Static constants used
-	EOF_FAT12 = 0x00000ff8
-	EOF_FAT16 = 0x0000fff8
-	EOF_FAT32 = 0x0ffffff8
-	# The size of a FAT directory entry
-	DIRSIZE = 32
+    # Static constants used
+    EOF_FAT12 = 0x00000ff8
+    EOF_FAT16 = 0x0000fff8
+    EOF_FAT32 = 0x0ffffff8
+    # The size of a FAT directory entry
+    DIRSIZE = 32
 
-	class Type:
-		FAT12 = 1
-		FAT16 = 2
-		FAT32 = 3
-		exFAT = 4
+    class Type:
+        FAT12 = 1
+        FAT16 = 2
+        FAT32 = 3
+        exFAT = 4
 
-	class Attribute:
-		READONLY = 0x01
-		HIDDEN = 0x02
-		SYSTEM = 0x04
-		LABEL = 0x08
-		DIRECTORY = 0x10
-		ARCHIVE = 0x20
-		LONGNAME = READONLY | HIDDEN | SYSTEM | LABEL
+    class Attribute:
+        READONLY = 0x01
+        HIDDEN = 0x02
+        SYSTEM = 0x04
+        LABEL = 0x08
+        DIRECTORY = 0x10
+        ARCHIVE = 0x20
+        LONGNAME = READONLY | HIDDEN | SYSTEM | LABEL
 
-	class FileNotFoundError(Exception):
-		def __init__(self, path):
-			self.path = path
-		def __str__(self):
-			return "The file or directory \"%s\" doesn't exist" % self.path
+    class FileNotFoundError(Exception):
+        def __init__(self, path):
+            self.path = path
+        def __str__(self):
+            return "The file or directory \"%s\" doesn't exist" % self.path
 
-	def __init__(self, fd):
-		self.fd = fd
-		self.__start = fd.tell()
-		self.info = self.__parse_bootsector()
+    def __init__(self, fd):
+        self.fd = fd
+        self.__start = fd.tell()
+        self.info = self.__parse_bootsector()
 
-		# Calculate the offset to the first FAT
-		self.__fat_start = self.__start + self.info["reserved_sectors"] * self.info["sector_size"]
+        # Calculate the offset to the first FAT
+        self.__fat_start = self.__start + self.info["reserved_sectors"] * self.info["sector_size"]
 
-		self.fat_type, self.EOF, self.__num_clusters = self.__determine_type()
+        self.fat_type, self.EOF, self.__num_clusters = self.__determine_type()
 
-		# Calculate the offset to the root directory
-		self.__root_dir = ((self.info["num_fats"] * self.info["sectors_per_fat"]) *
-			self.info["sector_size"]) + self.__fat_start
+        # Calculate the offset to the root directory
+        self.__root_dir = ((self.info["num_fats"] * self.info["sectors_per_fat"]) *
+            self.info["sector_size"]) + self.__fat_start
 
-		# Calculate the offset to the actual data start
-		self.__data_start = self.__root_dir + (FAT.DIRSIZE * self.info["root_entries"])
+        # Calculate the offset to the actual data start
+        self.__data_start = self.__root_dir + (FAT.DIRSIZE * self.info["root_entries"])
 
-	# Determines which type of FAT it is depending on the properties
-	def __determine_type(self):
-		root_dir_sectors = ((self.info["root_entries"] * FAT.DIRSIZE) +
-			(self.info["sector_size"] - 1)) / self.info["sector_size"]
-		data_sectors = self.info["total_sectors"] - (self.info["reserved_sectors"] +
-			(self.info["num_fats"] * self.info["sectors_per_fat"]) + root_dir_sectors)
-		num_clusters = data_sectors / self.info["sectors_per_cluster"]
-		if num_clusters < 4085:
-			return (FAT.Type.FAT12, FAT.EOF_FAT12, num_clusters)
-		elif num_clusters < 65525:
-			return (FAT.Type.FAT16, FAT.EOF_FAT16, num_clusters)
-		else:
-			return (FAT.Type.FAT32, FAT.EOF_FAT32, num_clusters)
+    # Determines which type of FAT it is depending on the properties
+    def __determine_type(self):
+        root_dir_sectors = ((self.info["root_entries"] * FAT.DIRSIZE) +
+            (self.info["sector_size"] - 1)) / self.info["sector_size"]
+        data_sectors = self.info["total_sectors"] - (self.info["reserved_sectors"] +
+            (self.info["num_fats"] * self.info["sectors_per_fat"]) + root_dir_sectors)
+        num_clusters = data_sectors / self.info["sectors_per_cluster"]
+        if num_clusters < 4085:
+            return (FAT.Type.FAT12, FAT.EOF_FAT12, num_clusters)
+        elif num_clusters < 65525:
+            return (FAT.Type.FAT16, FAT.EOF_FAT16, num_clusters)
+        else:
+            return (FAT.Type.FAT32, FAT.EOF_FAT32, num_clusters)
 
-	def __next_cluster(self, cluster):
-		offset = self.__fat_start
-		if self.fat_type == FAT.Type.FAT12:
-			offset += cluster + (cluster / 2)
-			self.fd.seek(offset, SEEK_SET)
-			value = unpack("<H", self.fd.read(2))[0]
-			return value >> 4 if cluster & 1 else value & 0xfff
-		elif self.fat_type == FAT.Type.FAT16:
-			offset += cluster * 2
-			self.fd.seek(offset, SEEK_SET)
-			return unpack("<H", self.fd.read(2))[0]
-		elif self.fat_type == FAT.Type.FAT32:
-			offset += cluster * 4
-			self.fd.seek(offset, SEEK_SET)
-			return unpack("<L", self.fd.read(4))[0]
-		else:
-			raise NotImplementedError
+    def __next_cluster(self, cluster):
+        offset = self.__fat_start
+        if self.fat_type == FAT.Type.FAT12:
+            offset += cluster + (cluster // 2)
+            self.fd.seek(offset, SEEK_SET)
+            value = unpack("<H", self.fd.read(2))[0]
+            return value >> 4 if cluster & 1 else value & 0xfff
+        elif self.fat_type == FAT.Type.FAT16:
+            offset += cluster * 2
+            self.fd.seek(offset, SEEK_SET)
+            return unpack("<H", self.fd.read(2))[0]
+        elif self.fat_type == FAT.Type.FAT32:
+            offset += cluster * 4
+            self.fd.seek(offset, SEEK_SET)
+            return unpack("<L", self.fd.read(4))[0]
+        else:
+            raise NotImplementedError
 
-	def next_free_cluster(self, start=2):
-		for i in range(start, self.__num_clusters):
-			cluster = self.__next_cluster(i)
-			if cluster == 0:
-				return i
-		return self.EOF
+    def next_free_cluster(self, start=2):
+        for i in range(start, self.__num_clusters):
+            cluster = self.__next_cluster(i)
+            if cluster == 0:
+                return i
+        return self.EOF
 
-	def get_cluster_chain(self, cluster):
-		chain = [cluster]
-		if cluster == 0:
-			return chain
-		while cluster < self.EOF:
-			chain.append(self.__next_cluster(cluster))
-			cluster = chain[-1]
-		return chain[:-1]
+    def get_cluster_chain(self, cluster):
+        chain = [cluster]
+        if cluster == 0:
+            return chain
+        while cluster < self.EOF:
+            chain.append(self.__next_cluster(cluster))
+            cluster = chain[-1]
+        return chain[:-1]
 
-	def read_cluster(self, cluster):
-		if cluster < 2:
-			return ""
-		self.fd.seek(self.cluster_to_offset(cluster))
-		return self.fd.read(self.info["sectors_per_cluster"] * self.info["sector_size"])
+    def read_cluster(self, cluster):
+        if cluster < 2:
+            return ""
+        self.fd.seek(self.cluster_to_offset(cluster))
+        return self.fd.read(self.info["sectors_per_cluster"] * self.info["sector_size"])
 
-	# Calculate the logical sector number from the cluster
-	def cluster_to_offset(self, cluster):
-		offset = ((cluster - 2) * self.info["sectors_per_cluster"]) * self.info["sector_size"]
-		return self.__data_start + offset
+    # Calculate the logical sector number from the cluster
+    def cluster_to_offset(self, cluster):
+        offset = ((cluster - 2) * self.info['sectors_per_cluster']) * self.info['sector_size']
+        return self.__data_start + offset
 
-	# Read everything we need from the bootsector
-	def __parse_bootsector(self):
-		data = unpack("<3x8sHBHBHHBHHHLL", self.fd.read(36))
-		return {
-			"oem": data[0].strip(" "),
-			"sector_size": data[1],
-			"sectors_per_cluster": data[2],
-			"reserved_sectors": data[3],
-			"num_fats": data[4],
-			"root_entries": data[5],
-			"total_sectors": data[6] if data[6] != 0 else data[12],
-			"media_descriptor": data[7],
-			"sectors_per_fat": data[8],
-			"sectors_per_track": data[9],
-			"num_heads": data[10],
-			"hidden_sectors": data[11]
-		}
+    # Read everything we need from the bootsector
+    def __parse_bootsector(self):
+        data = unpack("<3x8sHBHBHHBHHHLL", self.fd.read(36))
+        return {
+            'oem': data[0].decode('utf-8').strip(' '),
+            'sector_size': data[1],
+            'sectors_per_cluster': data[2],
+            'reserved_sectors': data[3],
+            'num_fats': data[4],
+            'root_entries': data[5],
+            'total_sectors': data[6] if data[6] != 0 else data[12],
+            'media_descriptor': data[7],
+            'sectors_per_fat': data[8],
+            'sectors_per_track': data[9],
+            'num_heads': data[10],
+            'hidden_sectors': data[11]
+        }
 
-	# Convert a FAT date to a date object
-	def __parse_fat_date(self, v):
-		year, month, day = 1980 + (v >> 9), (v >> 5) & 0x1f, v & 0x1f
-		month = min(max(month, 12), 1)
-		day = min(max(day, 31), 1)
-		return date(year, month, day)
+    # Convert a FAT date to a date object
+    def __parse_fat_date(self, v):
+        year, month, day = 1980 + (v >> 9), (v >> 5) & 0x1f, v & 0x1f
+        month = min(max(month, 12), 1)
+        day = min(max(day, 31), 1)
+        return date(year, month, day)
 
-	# Convert a FAT timestamp to a datetime object
-	def __parse_fat_datetime(self, v1, v2, v3):
-		hour, minute, second = v2 >> 11 & 0x1f, v2 >> 16 & 0x3f, (v2 & 0x1f) * 2
-		if v1 > 100:
-			second += 1
-			v1 -= 100
-		usec = v1 * 10000
-		d = self.__parse_fat_date(v3)
-		return datetime(d.year, d.month, d.day, hour, minute, second, usec)
+    # Convert a FAT timestamp to a datetime object
+    def __parse_fat_datetime(self, v1, v2, v3):
+        hour, minute, second = v2 >> 11 & 0x1f, v2 >> 16 & 0x3f, (v2 & 0x1f) * 2
+        if v1 > 100:
+            second += 1
+            v1 -= 100
+        usec = v1 * 10000
+        d = self.__parse_fat_date(v3)
+        return datetime(d.year, d.month, d.day, hour, minute, second, usec)
 
-	# Read and parse a FAT directory entry
-	def __read_dir_entry(self):
-		de = unpack("<11sBxBHHH2xHHHL", self.fd.read(FAT.DIRSIZE))
-		# TODO: Handle long filenames
-		if de[1] & FAT.Attribute.LONGNAME:
-			return None
-		else:
-			return {
-				"name": self.__normalize_name(de[0]),
-				"attributes": de[1],
-				"created": self.__parse_fat_datetime(de[2], de[3], de[4]),
-				"last_accessed": self.__parse_fat_date(de[5]),
-				"modified": self.__parse_fat_datetime(0, de[6], de[7]),
-				"cluster": de[8],
-				"size": de[9],
-				"direntry": self.fd.tell() - FAT.DIRSIZE
-			}
+    # Read and parse a FAT directory entry
+    def __read_dir_entry(self):
+        de = unpack("<11sBxBHHH2xHHHL", self.fd.read(FAT.DIRSIZE))
+        # TODO: Handle long filenames
+        if de[1] & FAT.Attribute.LONGNAME:
+            return None
+        else:
+            return {
+                "name": self.__normalize_name(de[0]),
+                "attributes": de[1],
+                "created": self.__parse_fat_datetime(de[2], de[3], de[4]),
+                "last_accessed": self.__parse_fat_date(de[5]),
+                "modified": self.__parse_fat_datetime(0, de[6], de[7]),
+                "cluster": de[8],
+                "size": de[9],
+                "direntry": self.fd.tell() - FAT.DIRSIZE
+            }
 
-	# Normalizes a 8.3 FAT filename
-	def __normalize_name(self, fatname):
-		if fatname[8:] == "   ":
-			# Skip the dot if there is no file extension
-			return fatname[:8].strip(" ")
-		else:
-			# Otherwise strip the spaces and dotify plus the extension
-			return fatname[:8].strip(" ") + "." + fatname[8:].strip(" ")
+    # Normalizes a 8.3 FAT filename
+    def __normalize_name(self, fatname):
+        fatname = fatname.decode('utf-8')
+        if fatname[8:] == '   ':
+            # Skip the dot if there is no file extension
+            return fatname[:8].strip(' ')
+        else:
+            # Otherwise strip the spaces and dotify plus the extension
+            #return fatname[:8].strip(' ') + '.' + fatname[8:].strip(' ')
+            name = fatname[:8].strip(' ') + '.' + fatname[8:].strip(' ')
+            return name if len(name) else '<spaces>'
 
-	def __read_dir(self, offset):
-		self.fd.seek(offset, SEEK_SET)
-		items = []
-		for i in range(self.info["root_entries"]):
-			de = self.__read_dir_entry()
-			if not de:
-				continue
-			# Skip deleted files
-			if de["name"][0] == '\xe9':
-				continue
-			# Break when we hit the first blank filename
-			elif de["name"][0] == '\x00':
-				break
-			else:
-				items.append(de)
-		return items
+    def __read_dir(self, offset):
+        self.fd.seek(offset, SEEK_SET)
+        items = []
+        for i in range(self.info["root_entries"]):
+            de = self.__read_dir_entry()
+            if not de:
+                continue
+            # Skip empty (blank?) names
+            if len(de['name']) == 0:
+                break
+            # Skip deleted files
+            if de['name'][0] == '\xe9':
+                continue
+            # Break when we hit the first blank filename
+            elif de['name'][0] == '\x00':
+                break
+            else:
+                items.append(de)
+        return items
 
-	def get_label(self):
-		# FIXME: Is the label always located as the first file in the root directory?
-		self.fd.seek(self.__root_dir, SEEK_SET)
-		return unpack("11s", self.fd.read(11))[0].strip(" ")
+    def get_label(self):
+        # FIXME: Is the label always located as the first file in the root directory?
+        self.fd.seek(self.__root_dir, SEEK_SET)
+        return unpack("11s", self.fd.read(11))[0].strip(" ")
 
-	def read_file(self, path):
-		path = path.lower()
-		pos = path.rfind("/")
-		items = self.read_dir("" if pos < 0 else path[:pos])
-		if items:
-			items = filter(lambda x: x["name"].lower() == path[pos+1:], items)
-			if items:
-				item = items[0]
-				data = "".join([self.read_cluster(c) for c in self.get_cluster_chain(item["cluster"])])
-				return data[:item["size"]]
-		raise FAT.FileNotFoundError(path)
+    def read_file(self, path):
+        path = path.lower()
+        pos = path.rfind("/")
+        items = self.read_dir("" if pos < 0 else path[:pos])
+        if items:
+            items = filter(lambda x: x["name"].lower() == path[pos+1:], items)
+            if items:
+                item = items[0]
+                data = "".join([self.read_cluster(c) for c in self.get_cluster_chain(item["cluster"])])
+                return data[:item["size"]]
+        raise FAT.FileNotFoundError(path)
 
-	def write_file(self, path):
-		raise NotImplementedError
+    def write_file(self, path):
+        raise NotImplementedError
 
-	def delete_file(self, path):
-		# Find the directory entry and replace the first character in the filenames
-		# with a 0xe5.  Then get the cluster chain and set all clusters to zero.
-		raise NotImplementedError
+    def delete_file(self, path):
+        # Find the directory entry and replace the first character in the filenames
+        # with a 0xe5.  Then get the cluster chain and set all clusters to zero.
+        raise NotImplementedError
 
-	# Read all files from a directory
-	def read_dir(self, path=""):
-		# Start with the root directory
-		items = self.__read_dir(self.__root_dir)
-		# Filter out empty strings
-		subdirs = filter(len, path.lower().split("/"))
-		# Now look in all sub directories for our path
-		for d in subdirs:
-			# Get the one and only directory we are looking for
-			items = filter(lambda x: x["attributes"] & FAT.Attribute.DIRECTORY and x["name"].lower() == d, items)
-			if not items:
-				raise FAT.FileNotFoundError(path)
-			items = self.__read_dir(self.cluster_to_offset(items[0]["cluster"]))
-		return items
+    # Read all files from a directory
+    def read_dir(self, path=''):
+        # Start with the root directory
+        items = self.__read_dir(self.__root_dir)
+        # Filter out empty strings
+        subdirs = [p.lower() for p in path.split('/') if len(p)]
+        # Now look in all sub directories for our path
+        for d in subdirs:
+            # Get the one and only directory we are looking for
+            items = list(filter(lambda x: x['attributes'] & FAT.Attribute.DIRECTORY and x['name'].lower() == d, items))
+            if not items:
+                raise FAT.FileNotFoundError(path)
+            items = self.__read_dir(self.cluster_to_offset(items[0]['cluster']))
+        return items
 
-	def set_attribute(self, path, attr):
-		slash = path.rfind("/")
-		filename = path
-		if slash > 0:
-			path, filename = path[:slash], path[slash+1:]
-		else:
-			path = "/"
+    def set_attribute(self, path, attr):
+        slash = path.rfind("/")
+        filename = path
+        if slash > 0:
+            path, filename = path[:slash], path[slash+1:]
+        else:
+            path = "/"
 
-		for f in self.read_dir(path):
-			if filename == f["name"]:
-				self.fd.seek(f["direntry"]+11, SEEK_SET)
-				self.fd.write(pack("B", attr))
-				break
+        for f in self.read_dir(path):
+            if filename == f["name"]:
+                self.fd.seek(f["direntry"]+11, SEEK_SET)
+                self.fd.write(pack("B", attr))
+                break
--- fattools.py
+++ fattools.py
@@ -2,81 +2,88 @@
 # -*- encoding: utf-8 -*-
 from fat import FAT
 import sys
-
+import argparse
 
 def get_all_files(fat, path=''):
-	files = fat.read_dir(path)
-	for f in files:
-		if f["name"] == '.' or f["name"] == "..":
-			continue
-		elif path:
-			f["name"] = "%s/%s" % (path, f["name"])
-		if f["attributes"] & FAT.Attribute.DIRECTORY:
-			files.extend(get_all_files(fat, "%s/%s" % (path, f["name"]) if path else f["name"]))
-	return files
+    files = fat.read_dir(path)
+    for f in files:
+        if f['name'] == '.' or f['name'] == '..':
+            continue
+        elif path:
+            f['name'] = f'{path}/{f["name"]}'
+        if f['attributes'] & FAT.Attribute.DIRECTORY:
+            files.extend(get_all_files(fat, f'{path}/{f["name"]}' if path else f['name']))
+    return files
 
 
 def get_file_from_sector(fat, sector):
-	files = filter(
-		lambda f: ord(f["name"][0]) != 0xe5 and
-			  f["name"] != '.' and
-			  f["name"] != "..",
-			get_all_files(fat)
-	)
-	for f in files:
-		for c in fat.get_cluster_chain(f["cluster"]):
-			cluster_sector = fat.cluster_to_offset(c) / fat.info["sector_size"]
-			if sector >= cluster_sector and sector < (cluster_sector + fat.info["sectors_per_cluster"]):
-				return f
+    files = filter(
+        lambda f: ord(f['name'][0]) != 0xe5 and
+              f['name'] != '.' and
+              f['name'] != '..',
+            get_all_files(fat)
+    )
+    for f in files:
+        for c in fat.get_cluster_chain(f['cluster']):
+            cluster_sector = fat.cluster_to_offset(c) / fat.info['sector_size']
+            if sector >= cluster_sector and sector < (cluster_sector + fat.info['sectors_per_cluster']):
+                return f
 
 
 def get_fragmented_files(fat):
-	fragmented_files = []
-	for f in get_all_files(fat):
-		chain = fat.get_cluster_chain(f["cluster"])
-		for i, c in enumerate(chain):
-			if len(chain) - 1 == i:
-				break
-			if c + 1 != chain[i + 1]:
-				fragmented_files.append(f)
-				break
-	return fragmented_files
+    fragmented_files = []
+    for f in get_all_files(fat):
+        chain = fat.get_cluster_chain(f['cluster'])
+        for i, c in enumerate(chain):
+            if len(chain) - 1 == i:
+                break
+            if c + 1 != chain[i + 1]:
+                fragmented_files.append(f)
+                break
+    return fragmented_files
 
+
+def parse_args():
+    formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=100, width=200)
+    parser = argparse.ArgumentParser(formatter_class=formatter_class)
+    parser.add_argument('image', help='FAT image')
+    parser.add_argument('-l', '--list', metavar='DIR', help='list files in directory')
+    parser.add_argument('-f', '--frag', action='store_true', help='list all fragmented files')
+    parser.add_argument('-s', '--sect', metavar='SECTOR', help='find out which file is on sector', type=int)
+    return parser.parse_args()
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return f'{num:3.1f}{unit}{suffix}'
+        num /= 1024.0
+    return f'{num:.1f}Yi{suffix}'
 
 def main():
-	commands = ["--list", "--frag", "--sect"]
+    args = parse_args()
 
-	if sys.argv[3:]:
-		print("Commands:")
-		print('\n'.join(commands))
-		print("fattools.py [--command] [argument] [image]")
-		sys.exit(1)
+    fat = FAT(open(args.image, 'rb'))
 
-	for i, arg in enumerate(argv):
-		if arg not in commands:
-			continue
-		
-		print(argv)
-		fat = FAT(open(argv[i+2], "rb"))
-		if arg == "--list":
-			print "List files"
-			return True
-		elif arg == "--frag":
-			fragmented_files = get_fragmented_files(fat)
-			for f in fragmented_files:
-				print(f["name"], " is fragmented")
-			return False
-		elif arg == "--sect":
-			sector = int(sys.argv[1])
-			f = get_file_from_sector(fat, sector)
-			if f:
-				for k, v in f.iteritems():
-					print("%-14.14s%s" % (k, v))
-				return False
-			print("Cannot find any file that occupies sector", sector)
-			return True
+    if args.list:
+        for f in get_all_files(fat, '' if args.list == '.' else args.list):
+            if f['attributes'] & FAT.Attribute.DIRECTORY:
+                print(f'{f["name"]:<30} <DIR>                      {f["last_accessed"]} {f["modified"]}')
+            else:
+                print(f'{f["name"]:<30} {sizeof_fmt(f["size"]):<10} cluster #{f["cluster"]:<6} {f["last_accessed"]} {f["modified"]}')
 
-		
-if __name__ == "__main__":
-	sys.exit(main())
+    if args.frag:
+        for f in get_fragmented_files(fat):
+            print(f'{f["name"]} is fragmented')
+
+    if args.sect:
+        f = get_file_from_sector(fat, args.sect)
+        if f:
+            for k, v in f.items():
+                print(f'{k+":":<15}{v}')
+        else:
+            print(f'Cannot find any file that occupies sector {args.sect}')
+        
+if __name__ == '__main__':
+    sys.exit(main())
 
